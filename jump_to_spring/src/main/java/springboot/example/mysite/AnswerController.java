d693ca6d202d94c24b21bb33c5eb5740d768eb50
groomigroom <googjohn0143@gmail.com>
2026-09-11 06:46:25 +0900

    Update AnswerController.java

--- jump_to_spring/src/main/java/springboot/example/mysite/AnswerController.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/AnswerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
+//클래스 내부에 final이나 @NonNull이 붙은 필드를 모아 생성자를 자동으로 만드는 거
 @Controller
 public class AnswerController {
 
