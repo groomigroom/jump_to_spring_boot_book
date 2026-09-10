ea26adbb3d1ca8f02dfc66337d3e459e0a698800
groomigroom <googjohn0143@gmail.com>
2026-09-11 06:48:41 +0900

    Update AnswerController.java

--- jump_to_spring/src/main/java/springboot/example/mysite/AnswerController.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/AnswerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 //클래스 내부에 final이나 @NonNull이 붙은 필드를 모아 생성자를 자동으로 만드는 거
 @Controller
+//클라이언트의 웹 요청을 받아 처리하고 뷰(View, HTML 화면 등)를 반환하는 클래스에 붙이는 거
 public class AnswerController {
 
     private final QuestionService questionService;
