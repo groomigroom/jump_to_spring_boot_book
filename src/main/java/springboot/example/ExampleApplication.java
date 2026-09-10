3cebd6a1da0dfe27c7da01694ceb921642b992be
groomigroom <googjohn0143@gmail.com>
2026-09-10 20:58:17 +0900

    Update ExampleApplication.java

--- src/main/java/springboot/example/ExampleApplication.java
+++ src/main/java/springboot/example/ExampleApplication.java
@@ -1,1 +1,13 @@
-d
+package springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
