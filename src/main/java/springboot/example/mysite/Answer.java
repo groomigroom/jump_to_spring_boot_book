eb6c000302e51f99a6c253a445e0cd461f58b69f
groomigroom <googjohn0143@gmail.com>
2026-09-10 20:59:28 +0900

    Update Answer.java

--- src/main/java/springboot/example/mysite/Answer.java
+++ src/main/java/springboot/example/mysite/Answer.java
@@ -1,1 +1,28 @@
-d
+package springboot.example.mysite;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+//    @Column(name = "groomgroom", length = 200)
+//    private String subject;
+
+    @Column(name = "groomigroom",columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "goooroooorom")
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
