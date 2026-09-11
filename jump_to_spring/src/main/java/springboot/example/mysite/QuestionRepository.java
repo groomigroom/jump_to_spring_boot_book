77da4dc251e2bbda21c7aa96bd1125ef14018a2e
groomigroom <googjohn0143@gmail.com>
2026-09-12 08:58:30 +0900

    Update QuestionRepository.java

--- jump_to_spring/src/main/java/springboot/example/mysite/QuestionRepository.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/QuestionRepository.java
@@ -1,1 +1,17 @@
-d
+package springboot.example.mysite;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+}
+
