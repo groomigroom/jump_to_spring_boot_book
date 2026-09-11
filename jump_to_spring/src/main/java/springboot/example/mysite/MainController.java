3af3ab055a8ea67f16a4042a4c63a29f5375486c
groomigroom <googjohn0143@gmail.com>
2026-09-12 08:53:13 +0900

    Update MainController.java

--- jump_to_spring/src/main/java/springboot/example/mysite/MainController.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/MainController.java
@@ -1,1 +1,19 @@
-d
+package springboot.example.mysite;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/kimgroomi")
+    @ResponseBody
+    public String index() {
+        return "안녕하세요. 김구름입니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+}
