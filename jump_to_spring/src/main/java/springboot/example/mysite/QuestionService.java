d8a1a5a2e0ca76f9bcf98b8d8c9668b028de8c08
groomigroom <googjohn0143@gmail.com>
2026-09-13 08:14:19 +0900

    Update QuestionService.java

--- jump_to_spring/src/main/java/springboot/example/mysite/QuestionService.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/QuestionService.java
@@ -65,6 +65,8 @@ public class QuestionService {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        //Pageable 페이징을 처리하는 인터페이스이다.
+        //PageRequest 현재 페이지와 한 페이지에 보여 줄 게시물 개수 등을 설정하여 페이징 요청을 하는 클래스이다.
         Specification<Question> spec = search(kw);
         return this.questionRepository.findAll(spec, pageable);
     }
