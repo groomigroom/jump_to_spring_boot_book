cbad96f1bebb2dbe4cd10f04e948b96ed534ef95
groomigroom <googjohn0143@gmail.com>
2026-09-12 09:05:53 +0900

    Add UserRepository interface for SiteUser entity

--- jump_to_spring/src/main/java/springboot/example/mysite/UserRepository.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/UserRepository.java
@@ -1,1 +1,9 @@
-s
+package springboot.example.mysite;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
+}
