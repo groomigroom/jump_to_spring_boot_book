1813dd9361a8344a3a45d9041525e19f9ad68f40
groomigroom <googjohn0143@gmail.com>
2026-09-12 19:11:20 +0900

    Update DataNotFoundException.java

--- jump_to_spring/src/main/java/springboot/example/mysite/DataNotFoundException.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/DataNotFoundException.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "entity not found")
+//@ResponseStatus는 HTTP 요청이 성공하거나 실패했을 때 클라이언트에게 보낼 HTTP 응답 상태 코드(Status Code)를 직접 지정하는 어노테이션입니다.
+    
 public class DataNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
     public DataNotFoundException(String message) {
