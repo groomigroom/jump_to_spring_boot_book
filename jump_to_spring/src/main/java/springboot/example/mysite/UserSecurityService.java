fb36175c718dcac94794e4289f7640a81639ddd4
groomigroom <googjohn0143@gmail.com>
2026-09-14 08:24:50 +0900

    Update UserSecurityService.java

--- jump_to_spring/src/main/java/springboot/example/mysite/UserSecurityService.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/UserSecurityService.java
@@ -26,6 +26,7 @@ public class UserSecurityService implements UserDetailsService {
         //UsernameNotFoundException은 스프링 시큐리티에서 "로그인을 시도할 때, 사용자가 입력한 아이디(Username)에 해당하는 유저를 데이터베이스나 시스템에서 찾을 수 없을 때" 발생하는 예외입니다.
         Optional<SiteUser> _siteUser = this.userRepository.findByusername(username);
         if (_siteUser.isEmpty()) {
+            //Optional에 .isEmpty()를 호출하는 것은 "이 객체 안에 데이터가 비어있는지(null인지)" 확인하는 기능입니다.
             throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
         }
         SiteUser siteUser = _siteUser.get();
