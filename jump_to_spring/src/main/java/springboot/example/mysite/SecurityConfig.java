0cb72e5d599304e691c9f288db0b82155df8d419
groomigroom <googjohn0143@gmail.com>
2026-09-13 14:31:16 +0900

    Add comment for BCryptPasswordEncoder in SecurityConfig
    Add comment explaining BCryptPasswordEncoder usage.

--- jump_to_spring/src/main/java/springboot/example/mysite/SecurityConfig.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+        //BCryptPasswordEncoder는 해시 암호화하는거
     }
 
     @Bean
