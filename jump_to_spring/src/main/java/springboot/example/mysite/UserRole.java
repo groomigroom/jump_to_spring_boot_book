c2b30afc641fb9b5a12e70e9f99cda236ac838db
groomigroom <googjohn0143@gmail.com>
2026-09-14 08:11:17 +0900

    Update UserRole.java

--- jump_to_spring/src/main/java/springboot/example/mysite/UserRole.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/UserRole.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum UserRole {
+    //Enum은 상수(변하지 않는 값)들의 집합을 정의할 때 사용합니다. 권한의 종류는 제한적이고 명확하므로(관리자, 회원 등) 텍스트나 숫자 대신 Enum을 사용하면 오타로 인한 버그를 원천 차단할 수 있습니다.
     ADMIN("ROLE_ADMIN"),
     USER("ROLE_USER");
 
