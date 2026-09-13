b2a3f40eb825c382e8a750b9fc25b2c7492bd5c7
groomigroom <googjohn0143@gmail.com>
2026-09-14 08:06:54 +0900

    Update UserRepository.java

--- jump_to_spring/src/main/java/springboot/example/mysite/UserRepository.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/UserRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
     Optional<SiteUser> findByusername(String username);
+    //자바 스프링 부트에서 Optional은 "값이 비어있을 수도 있고, 존재할 수도 있는 상태"를 나타내는 컨테이너 객체입니다.
 }
