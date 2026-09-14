585d748314dfefd46c7491ffda5f9be31d1c9e44
groomigroom <googjohn0143@gmail.com>
2026-09-14 16:55:50 +0900

    Enhance comments in CommonUtil.java
    Add comment explaining HtmlRenderer functionality.

--- jump_to_spring/src/main/java/springboot/example/mysite/CommonUtil.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/CommonUtil.java
@@ -15,6 +15,7 @@ public class CommonUtil {
         //Node는 마크다운 텍스트를 이루는 개별 구성 요소(제목, 문단, 링크, 이미지 등)를 추상화한 최상위 자바 객체(클래스)
         //parser.parse() 마크다운 문자열을 분석하여 객체 트리(Node)로 만듭니다.
         HtmlRenderer renderer = HtmlRenderer.builder().build();
+        //HtmlRenderer는 마크다운 구조(Node 트리)를 웹 브라우저가 읽을 수 있는 HTML 태그 문자열로 최종 변환(렌더링)해 주는 출력기 클래스입니다.
         return renderer.render(document);
     }
 }
