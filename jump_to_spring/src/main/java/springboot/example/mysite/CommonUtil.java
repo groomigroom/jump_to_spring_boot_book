ced0d3a5edd0e76b2656542bc0c0e523fe8ee894
groomigroom <googjohn0143@gmail.com>
2026-09-14 16:50:40 +0900

    Update CommonUtil.java

--- jump_to_spring/src/main/java/springboot/example/mysite/CommonUtil.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/CommonUtil.java
@@ -10,6 +10,7 @@ public class CommonUtil {
     public String markdown(String markdown) {
         Parser parser = Parser.builder().build();
         //commonmark-java가 제공하는 구문 분석기(Parser) 클래스
+        //.builder()는 빌더 패턴(Builder Pattern)을 구현하여 객체를 안전하고 가독성 있게 생성하는 메서드입니다.
         Node document = parser.parse(markdown);
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         return renderer.render(document);
