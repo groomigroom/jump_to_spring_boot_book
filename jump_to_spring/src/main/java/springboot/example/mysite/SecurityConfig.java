8b4ad18c81a43889bde92efb97247ea584f0735a
groomigroom <googjohn0143@gmail.com>
2026-09-14 10:30:29 +0900

    Update SecurityConfig.java

--- jump_to_spring/src/main/java/springboot/example/mysite/SecurityConfig.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/SecurityConfig.java
@@ -59,6 +59,8 @@ public class SecurityConfig {
                 //.logoutRequestMatcher(PathPatternRequestMatcher.pathPattern(HttpMethod.GET, "/user/logout"))역할: 로그아웃을 실행할 특정 URL 주소와 HTTP 메서드를 지정합니다.
                 //PathPatternRequestMatcher.pathPattern(...): 스프링 부트에서 경로를 매칭할 때 쓰는 고성능 경로 매칭 방식입니다.
                 //HttpMethod.GET: 로그아웃 요청을 GET 방식으로 받겠다고 명시합니다.
+                //.logoutSuccessUrl("/")역할: 로그아웃이 성공적으로 완료된 후 사용자를 이동시킬(리다이렉트) URL 주소를 지정합니다.
+                
         ;
         return http.build();
         //.build()는 "지금까지 채워 넣은 설정 데이터를 바탕으로, 최종적으로 사용할 실제 객체를 완성해서 만들어내라"는 명령입니다. 이는 자바 디자인 패턴 중 하나인 빌더 패턴(Builder Pattern)의 마지막 단계에 해당하는 메서드입니다.
