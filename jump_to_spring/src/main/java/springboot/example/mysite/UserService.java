f754e1a38da63527c19211ab4b29566e9b690eeb
groomigroom <googjohn0143@gmail.com>
2026-09-14 11:02:16 +0900

    Update UserService.java

--- jump_to_spring/src/main/java/springboot/example/mysite/UserService.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/UserService.java
@@ -26,6 +26,7 @@ public class UserService {
     public SiteUser getUser(String username) {
         Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
         if (siteUser.isPresent()) {
+            //isPresent(): 값이 들어있는지 여부를 true/false로 확인합니다. (다만, 이 메서드를 통한 if문 분기는 옛날 방식이므로 orElseThrow나 ifPresent 사용을 더 권장합니다.) 
             return siteUser.get();
         } else {
             throw new DataNotFoundException("siteuser not found");
