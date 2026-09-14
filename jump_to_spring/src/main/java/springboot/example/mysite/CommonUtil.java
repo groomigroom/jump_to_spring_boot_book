1b0c11ecd60b324b6d2c58e107a691f364c26976
groomigroom <googjohn0143@gmail.com>
2026-09-14 16:54:41 +0900

    Enhance comments in CommonUtil.java
    Added comment to explain parser.parse() method.

--- jump_to_spring/src/main/java/springboot/example/mysite/CommonUtil.java
+++ jump_to_spring/src/main/java/springboot/example/mysite/CommonUtil.java
@@ -13,6 +13,7 @@ public class CommonUtil {
         //.builder()는 빌더 패턴(Builder Pattern)을 구현하여 객체를 안전하고 가독성 있게 생성하는 메서드입니다.
         Node document = parser.parse(markdown);
         //Node는 마크다운 텍스트를 이루는 개별 구성 요소(제목, 문단, 링크, 이미지 등)를 추상화한 최상위 자바 객체(클래스)
+        //parser.parse() 마크다운 문자열을 분석하여 객체 트리(Node)로 만듭니다.
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         return renderer.render(document);
     }
